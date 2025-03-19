5ba5ab49bdb61a56e363b4bba76457575bd8c05e
cena <sina.jafari1970@gmail.com>
2025-03-20 01:11:41 +0330

    patch: add inventory

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'inventory' => [
+        'url' => env("INVENTORY_URL", "http://127.0.0.1:9001")
+    ],
 
 ];
