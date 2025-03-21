1bf762f515a8eabeb3553bc85435dca77a40be9d
cena <s.jafari@asiatech.cloud>
2025-03-22 02:42:23 +0330

    add observer and kafka producer

--- app/Models/OrderEvent.php
+++ app/Models/OrderEvent.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\OrderEventObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+#[ObservedBy([OrderEventObserver::class])]
 class OrderEvent extends Model
 {
     use HasFactory;
---
+++ app/Observers/OrderEventObserver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\OrderEvent;
+use Junges\Kafka\Facades\Kafka;
+use Junges\Kafka\Message\Message;
+
+class OrderEventObserver
+{
+    /**
+     * Handle the OrderEvent "created" event.
+     */
+    public function created(OrderEvent $orderEvent): void
+    {
+        //create event for message kafka service
+        $message = new Message(
+            body: [
+                "userId" => $orderEvent->order->user_id,
+                "status" => $orderEvent->status,
+                "payload" => $orderEvent->payload,
+                "source" => "Order Service"
+            ]
+        );
+        Kafka::publish('broker_name')->onTopic('toic_name')->withMessage($message)->send();
+    }
+
+    /**
+     * Handle the OrderEvent "updated" event.
+     */
+    public function updated(OrderEvent $orderEvent): void
+    {
+        $message = new Message(
+            body: [
+                "userId" => $orderEvent->order->user_id,
+                "status" => $orderEvent->status,
+                "payload" => $orderEvent->payload,
+                "source" => "Order Service"
+            ]
+        );
+        Kafka::publish('broker_name')->onTopic('toic_name')->withMessage($message)->send();
+    }
+
+    /**
+     * Handle the OrderEvent "deleted" event.
+     */
+    public function deleted(OrderEvent $orderEvent): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderEvent "restored" event.
+     */
+    public function restored(OrderEvent $orderEvent): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderEvent "force deleted" event.
+     */
+    public function forceDeleted(OrderEvent $orderEvent): void
+    {
+        //
+    }
+}
