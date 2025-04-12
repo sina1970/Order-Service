5bbc47c02e025e5e335d95a81dd138837e7d4354
cena <sina.jafari1970@gmail.com>
2025-04-12 21:04:40 +0330

    feat: create cart controller and service

---
+++ app/Http/Controllers/Admin/V1/CartController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\V1;
+
+use App\Http\Controllers\Controller;
+
+class CartController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+}
